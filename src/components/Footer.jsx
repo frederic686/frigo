05124be1b19d4949274deeb3bde76666659ef0f6
frederic686 <frederic.maripalane@hotmail.fr>
2025-08-18 23:58:24 +0200

    compteur

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,29 @@
 // src/components/Footer.jsx
 import "./footer.css";
+import { useDataRecette } from "../data/DataRecette.jsx";
 
 export default function Footer() {
+  const { favoris } = useDataRecette();
+
   return (
     <div className="footer">
       <nav>
         <a href="/" aria-label="Accueil">
           <span className="icon icon--home" />
         </a>
+
         <a href="/recettes" aria-label="Recettes">
           <span className="icon icon--tab" />
         </a>
-        <a href="/favoris" aria-label="Favoris">
+
+        <a href="/favoris" aria-label="Favoris" className="fav-link">
           <span className="icon icon--fav" />
+          {favoris.length > 0 && (
+            <span className="badge">{favoris.length}</span>
+          )}
         </a>
       </nav>
     </div>
   );
 }
+
